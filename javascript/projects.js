7cced09458576486faadca27f2b487ef733d9275
Rayane Sefiani <ryanawex@gmail.com>
2025-03-18 02:51:20 +0000

    Update projects.js

--- javascript/projects.js
+++ javascript/projects.js
@@ -8,7 +8,7 @@ export const projects = [
       Das ist ein einfaches YouTube-Projekt. Ich habe einige Änderungen am Layout vorgenommen und JavaScript verwendet, um die Inhaltsdaten anzuzeigen, um die Leistung zu verbessern, anstatt normalen HTML-Code zu nutzen. Dieses Projekt wurde mit HTML, CSS und JavaScript entwickelt.
     `,
     buttonView: `Youtube-Project/index.html`,
-    buttonCode: `https://github.com/RyanAwex/PlayTube`
+    buttonCode: `https://github.com/RyanAwex/Portfolio/tree/main/Youtube-Project`
   },
   {
     image: `images/todo-list.png`,
@@ -19,7 +19,7 @@ export const projects = [
       Dies ist ein To-Do-List-Projekt, bei dem einige leistungsstarke Funktionen hinzugefügt wurden. Es kann die Daten, die du eingibst, im localStorage speichern, um sie jederzeit anzuzeigen, mit einer Lösch- und einer Häkchen-Schaltfläche. In diesem Projekt wurden HTML, CSS und JavaScript verwendet.
     `,
     buttonView: `To-Do-List/index.html`,
-    buttonCode: `https://github.com/RyanAwex/To-Do-List`
+    buttonCode: `https://github.com/RyanAwex/Portfolio/tree/main/To-Do-List`
   },
   {
     image: `images/rps.jpg`,
@@ -32,7 +32,7 @@ export const projects = [
       Dies ist ein Schere, Stein, Papier-Spiel, das mit HTML, CSS und JavaScript entwickelt wurde. Dieses Spiel bietet ein interaktives Erlebnis mit einer Punktverfolgung und einer Auto-Play-Funktion, die alle 1 Sekunde für automatisiertes Gameplay läuft, sowie einer "Punktestand zurücksetzen"-Schaltfläche, die den Punktestand auf Null setzt.
     `,
     buttonView: `Rock-Paper-Scissors-Game/index.html`,
-    buttonCode: `https://github.com/RyanAwex/rock-paper-scissors-game`
+    buttonCode: `https://github.com/RyanAwex/Portfolio/tree/main/Rock-Paper-Scissors-Game`
   },
   {
     image: `images/calculator.png`,
@@ -47,7 +47,7 @@ export const projects = [
       Dies ist eine einfache Taschenrechner-App mit einem modernen Design, die leicht für alltägliche Mathematik wie Addieren, Subtrahieren, Multiplizieren und Dividieren zu verwenden ist. Sie hat eine saubere und intuitive Benutzeroberfläche, was sie zu einem großartigen Werkzeug für alle macht, die grundlegende Berechnungen schnell und einfach durchführen müssen.
     `,
     buttonView: `Calculator-Project/index.html`,
-    buttonCode: `https://github.com/RyanAwex/calculator`
+    buttonCode: `https://github.com/RyanAwex/Portfolio/tree/main/Calculator-Project`
   },
   {
     image: `images/password.png`,
@@ -59,6 +59,6 @@ export const projects = [
       Dies ist ein Passwort-Generator-Projekt. Wenn man auf die Schaltfläche "Generieren" klickt, erzeugt der Algorithmus ein zufälliges Passwort mit einer festgelegten Anzahl an Zahlen, Buchstaben und Symbolen. Dieses Projekt bietet auch eine "Kopieren"-Schaltfläche, mit der man das generierte Passwort einfach kopieren kann.
     `,
     buttonView: `Password-Generator/index.html`,
-    buttonCode: `https://github.com/RyanAwex/Password-Generator`
+    buttonCode: `https://github.com/RyanAwex/Portfolio/tree/main/Password-Generator`
   }
 ];
