a651c4b6c4d845743055d0638045533a6b5fc48f
RyanAwex <ryanawex@gmail.com>
2025-12-04 21:25:37 +0100

    edit map()

--- api/skills.js
+++ api/skills.js
@@ -1,8 +1,7 @@
 import { neon } from "@neondatabase/serverless";
 
-// TODO: Replace with your actual Neon DB connection string
 const sql = neon(
-  "postgresql://USERNAME:PASSWORD@HOST/DATABASE?sslmode=require"
+  "postgresql://neondb_owner:npg_uSLC6ciDr9YN@ep-old-dream-adclu4jq-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 );
 
 export default async function handler(req, res) {
