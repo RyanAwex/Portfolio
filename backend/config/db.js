624817ffe57989072a7fec5618d86e0572132ef4
RyanAwex <ryanawex@gmail.com>
2025-12-04 21:18:11 +0100

    edit map()

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,5 +3,5 @@ import { neon } from "@neondatabase/serverless";
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
 export const sql = neon(
-  "postgresql://neondb_owner:npg_uSLC6ciDr9YN@ep-old-dream-adclu4jq-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+  `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`
 );
