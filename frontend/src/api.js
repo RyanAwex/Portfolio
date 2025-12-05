b6ace544e8ebf23285ab64bed9ef69314324379f
RyanAwex <ryanawex@gmail.com>
2025-12-05 15:28:54 +0100

    trying to deploy it on cloudeflare

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,4 @@
 export const API =
-  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api"
+    : "https://portfolio-pi-mocha-go22e9136w.vercel.app/api";
