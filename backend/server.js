937ffc3198afab79146265c21721a01ba338c47b
RyanAwex <ryanawex@gmail.com>
2025-12-06 14:30:13 +0100

    add frontend access

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://frontend-portfolio-six-weld.vercel.app",
+      "https://rayanesefiani.com",
+    ],
+    credentials: true,
+  })
+);
 
 app.use("/api/projects", projectsRoute);
 app.use("/api/skills", skillsRoute);
