f97998a5be0168b9d8a66e667be2c36828731303
RyanAwex <ryanawex@gmail.com>
2026-01-02 22:06:43 +0100

    change the portfolio layout

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
 export const API =
-  import.meta.env.MODE === "development"
+  import.meta.env.VITE_MODE === "development"
     ? "http://localhost:5000/api"
     : "https://portfolio-pi-mocha-go22e9136w.vercel.app/api";
