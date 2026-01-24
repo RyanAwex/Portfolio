3e3381bf7de1041de89802e5793b061a110c77f7
RyanAwex <ryanawex@gmail.com>
2026-01-24 18:11:50 +0100

    add domain name again

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(
         "https://www.ryanawex.site",
       ].filter(Boolean);
       if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) return callback(null, true);
+      if (allowedOrigins.includes(origin)) return callback(null, origin);
       return callback(new Error("Not allowed by CORS"));
     },
     credentials: true,
